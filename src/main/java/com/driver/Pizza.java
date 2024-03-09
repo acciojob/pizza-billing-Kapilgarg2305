b996b68ac61d8ffd98f22a9837a341cac208ebff
Kapil <kapilgargymca@gmail.com>
2024-03-10 01:10:29 +0530

    a1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -4,10 +4,20 @@ public class Pizza {
 
     private int price;
     private Boolean isVeg;
-    private String bill;
+    private int bill;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+       if(isVeg) {
+           System.out.println("Base Price Of The Pizza: 300");
+           price=300;
+           bill=300;
+       }
+       else {
+           System.out.println("Base Price Of The Pizza: 400");
+           price =400;
+           bill= 400;
+       }
         // your code goes here
     }
 
@@ -17,18 +27,36 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        price=80;
+        bill=bill+80;
+        System.out.println("Extra Cheese Added: 80");
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg) {
+            System.out.println("Extra Toppings Added: 70");
+           price= 70;
+            bill=bill+70;
+        }
+        else {
+            System.out.println("Extra Toppings Added: 120");
+            price=120;
+            bill=bill+120;
+        }
+
     }
 
     public void addTakeaway(){
         // your code goes here
+        price =20;
+        bill=bill+20;
+        System.out.println("Paperbag Added: 20");
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+
+        return "Total Price: "+this.bill;
     }
 }
